9231d46c666596281a1569baec697fe7eec5e8bb
t.toropova17 <93729665+ttoropova17@users.noreply.github.com>
2022-04-02 23:56:10 +0400

    Upload files

---
+++ script.js
@@ -0,0 +1,27 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+});
+let mockDataAuto = {
+    mazda: {
+        3: {
+            carpetsFour: 1300,
+            carpetsTrunk: 1200,
+        },
+        6: {
+            carpetsFour: 1300,
+            carpetsTrunk: 1200,
+        }
+    }
+};
+function generateNewChoice () {
+
+}
+function getNewChoice (val) {
+    switch (val) {
+        case 1:
+
+    }
+}
